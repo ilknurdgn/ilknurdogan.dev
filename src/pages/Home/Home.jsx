b772f21ccd2ceeda6d590dde0fd5e8308e09363a
İlknur Doğan <ilknurdogan254@gmail.com>
2024-09-23 16:43:23 +0300

    feat(Home.jsx): add Projects component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../../components/about/About";
 import { Skills } from "../../components/skills/Skills";
+import { Projects } from "../../components/projects/Projects";
 
 const Home = () => {
   return (
     <div>
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 };
