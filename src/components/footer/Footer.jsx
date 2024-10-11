6338bab2395c25f24915243919e3720c1694b42c
İlknur Doğan <ilknurdogan254@gmail.com>
2024-10-11 11:06:29 +0300

    feat: create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./footer.module.css";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 - Durdu Ilknur Dogan</p>
+    </footer>
+  );
+};
+
+export default Footer;
