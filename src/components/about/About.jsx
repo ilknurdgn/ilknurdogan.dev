460f9c36496775eeeef09f81c6730ad18d47f38b
İlknur Doğan <ilknurdogan254@gmail.com>
2024-11-01 16:21:34 +0300

    project completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,38 +6,41 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
-import Typewriter from "typewriter-effect/dist/core";
+import Typed from "typed.js";
 
 const About = () => {
-  // const typewriterRef = useRef(null);
+  const typedRef = useRef(null);
 
-  // useEffect(() => {
-  //   const typewriter = new Typewriter(typewriterRef.current, {
-  //     loop: true,
-  //     delay: 200,
-  //   });
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: ["Durdu Ilknur Dogan", "Web Developer"],
+      typeSpeed: 70,
+      backSpeed: 50,
+      loop: true,
+    });
 
-  //   typewriter
-  //     .typeString("Backend Developer")
-  //     .pauseFor(1000)
-  //     .deleteAll()
-  //     .start();
-  // }, []);
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <div id="about" className={styles["about-section"]}>
+      <div  className={styles["about-container"]}>
       <div className={styles["about-content"]}>
         <div className={styles["about-text"]}>
-          <h1>Durdu Ilknur Dogan</h1>
-          {/* <h2 ref={typewriterRef}></h2> */}
+          <h1>
+            Hi there, <br />
+            I'm <span ref={typedRef}></span>{" "}
+          </h1>
           <p>
-            3rd grade Computer Engineering Student at Pamukkale University and
-            core-team member of PauSiber. I'm interested in Web Development and
-            especially working on backend technologies.
+            I'm a 3rd year Computer Engineering student at Pamukkale University.
+            I continue to improve myself in the field of web development,
+            especially focusing on backend technologies.
           </p>
         </div>
         <div className={styles["about-contact"]}>
-          <a title="CV" href="/doc/cv_Durdu_İlknur_Doğan.pdf" target="_blank">
+          <a title="CV" href="/doc/DurduIlknurDogan_CV.pdf" target="_blank">
             <IoDocumentText />
           </a>
           <a
@@ -72,6 +75,7 @@ const About = () => {
 
       <div className={styles["about-image"]}>
         <img src="/images/me.jpg" alt="" />
+      </div>
       </div>
     </div>
   );
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./projects.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,79 +9,86 @@ import "swiper/css/navigation";
 import "./styles.css";
 
 import { FreeMode, Pagination, Navigation } from "swiper/modules";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 export const Projects = () => {
+  const projects = [
+    {
+      title: "VANCA",
+      description:
+        "This application is a platform where users can filter, review and rent the caravans they want.",
+      image: "/images/projects/vanca.png",
+      link: "https://github.com/ilknurdgn/caravan-rental-app",
+    },
+    {
+      title: "ServiceHive",
+      description:
+        "This project is an application where customers can filter and find service providers for home services and submit service requests.",
+      image: "/images/projects/serviceHive.png",
+      link: "https://github.com/ilknurdgn/ServiceHive",
+    },
+    {
+      title: "Blog App",
+      description:
+        "This project is a full blogging application where users can create accounts and post blog posts",
+      image: "/images/projects/blogApp.png",
+      link: "https://github.com/ilknurdgn/blog-app",
+    },
+    {
+      title: "Weather App",
+      description:
+        "This application shows the weather of the city you want. I used Vanilla JS and Fetch API while making this application.",
+      image: "/images/projects/weatherApp.png",
+      link: "https://github.com/ilknurdgn/weather-app-js",
+    },
+    {
+      title: "Todo List",
+      description:
+        "This is a to do list website which I make using HTML, CSS and JavaScript.",
+      image: "/images/projects/todolist.png",
+      link: "https://github.com/ilknurdgn/javascript-todolist",
+    },
+  ];
+
   return (
     <div id="projects" className={styles["projects-section"]}>
       <h1 className={styles["projects-title"]}>Projects</h1>
       <Swiper
-        slidesPerView={3}
+        slidesPerView={2}
         spaceBetween={20}
         freeMode={true}
         pagination={{
           clickable: true,
         }}
+        grabCursor={true}
         navigation={true}
         modules={[FreeMode, Pagination, Navigation]}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          770: {
+            slidesPerView: 2,
+          },
+        }}
         className="mySwiper"
       >
-        <div>
-          <SwiperSlide>
-            <div className="image-container">
-              <img src="/images/todolist.png" alt="" />
+        {projects.map((project, index) => (
+          <SwiperSlide key={index}>
+            <div className={styles["image-container"]}>
+              <img src={project.image} alt={project.title} />
             </div>
-            <p>
-              <h2>VANCA</h2>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-              voluptatibus beatae quos quibusdam ea quia laudantium ducimus
-              facilis.
-            </p>
-          </SwiperSlide>
-        </div>
-
-        <div>
-          <SwiperSlide>
-            <div className="image-container">
-              <img src="/images/quizApp.png" alt="" />
+            <div className={styles["project-desc"]}>
+              <a href={project.link} className={styles["project-title"]}>
+                <h2>{project.title}</h2>
+                <i>
+                  <FaExternalLinkAlt />
+                </i>
+              </a>
+              <p>{project.description}</p>
             </div>
-            <p>
-              <h2>VANCA</h2>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-              voluptatibus beatae quos quibusdam ea quia laudantium ducimus
-              facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Neque voluptatibus beatae quos quibusdam ea quia laudantium
-              ducimus facilis.
-            </p>
           </SwiperSlide>
-        </div>
-
-        <div>
-          <SwiperSlide>
-            <div className="image-container">
-              <img src="/images/weatherApp.png" alt="" />
-            </div>
-            <p>
-              <h2>VANCA</h2>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-              voluptatibus beatae quos quibusdam ea quia laudantium ducimus
-              facilis.
-            </p>
-          </SwiperSlide>
-        </div>
-
-        <div>
-          <SwiperSlide>
-            <div className="image-container">
-              <img src="/images/chatApp.png" alt="" />
-            </div>
-            <p>
-              <h2>VANCA</h2>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-              voluptatibus beatae quos quibusdam ea quia laudantium ducimus
-              facilis.
-            </p>
-          </SwiperSlide>
-        </div>
+        ))}
       </Swiper>
     </div>
   );
